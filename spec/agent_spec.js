6c50c6c02d55c365e06cde1e5428c3e6ddb0a4b6
Stephen R. Veit <sveit@tradeharbor.com>
2011-08-12 16:49:40 -0500

    add specs for agent

--- spec/agent_spec.js
+++ spec/agent_spec.js
@@ -3,10 +3,12 @@ i = require('sys').inspect;
 
 
 describe("Mechanize/Agent", function () {
-  var agent, options, response, responseBody, requestOptions;
+  var agent, options, response, responseBody, requestOptions, responseErr, 
+  responsePage;
 
   beforeEach(function () {
     agent = new Agent();
+    agent.userAgentAlias = 'My agent';
     requestOptions = null;
     response = {
       statusCode: '200'
@@ -20,8 +22,23 @@ describe("Mechanize/Agent", function () {
     };
   });
 
+  context("getting page", function () {
+    beforeEach(function () {
+      agent.get({uri: 'http://example.com/index.html'}, function (err, page) {
+        responseErr = err;
+        responsePage = page;
+      });
+    });
+
+    it("should have referer", function () {
+      console.log("requestOptions: " + require('sys').inspect(requestOptions));
+//      requestOptions.headers.Host.should.eql('example.com');
+    });
+
+  });
+
   context("submitting form", function () {
-    var form, submitErr, submitPage;
+    var form, submitErr, submitPage, referrer, contentType;
 
     beforeEach(function () {
       form = {
@@ -34,13 +51,16 @@ describe("Mechanize/Agent", function () {
 
     context("with partial URL", function () {
       beforeEach(function () {
+        referrer = "http://example.com/page";
         form.action = 'login';
-        form.page = {uri: "http://example.com/page"};
+        form.page = {uri: referrer};
       });
 
       context("with POST method", function () {
         beforeEach(function () {
+          contentType = 'application/x-www-form-urlencoded';
           form.method = 'POST';
+          form.enctype = contentType;
           agent.submit(form, function (err, page) {
             submitErr = err;
             submitPage = page;
@@ -50,6 +70,27 @@ describe("Mechanize/Agent", function () {
         it("should use URI", function () {
           requestOptions.uri.should.eql('http://example.com/login');
         });
+
+        it("should have referer", function () {
+          requestOptions.headers.Referer.should.eql(referrer);
+        });
+
+        it("should have origin", function () {
+          requestOptions.headers.Origin.should.eql('http://example.com');
+        });
+
+        it("should have user agent", function () {
+          requestOptions.headers['User-Agent'].should.eql('My agent');
+        });
+
+        it("should have content type", function () {
+          requestOptions.headers['Content-Type'].should.eql(contentType);
+        });
+
+        it("should have content length", function () {
+          requestOptions.headers['Content-Length'].should.eql('0');
+        });
+
       });
     });
 
@@ -71,23 +112,18 @@ describe("Mechanize/Agent", function () {
           requestOptions.uri.should.eql('http://example.com/login');
         });
 
-  
         it("should post form", function () {
           requestOptions.method.should.eql('POST');
         });
+
         // Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
         // Accept-Charset:ISO-8859-1,utf-8;q=0.7,*;q=0.3
         // Accept-Encoding:gzip,deflate,sdch
         // Accept-Language:en-US,en;q=0.8
         // Cache-Control:max-age=0
         // Connection:keep-alive
-        // Content-Length:11682
-        // Content-Type:application/x-www-form-urlencoded
         // Cookie:__utmz=61007678.1305596160.17.2.utmccn=(referral)|utmcsr=banking.commercebank.com|utmcct=/CBI/login.aspx|utmcmd=referral; PMData=PMV5DWe%2FsxKTBOtqeUNOOdquqzzk7sbQ5DBq7xcybZGU%2FV55aZUkH9ZnDKWCeGaxEpzywJESltItBZ3P2H%2FjAb4Kmq5g%3D%3D; __utma=61007678.1044561854.1294586917.1307199932.1307209924.21; __utmb=61007678; __utmc=61007678; TestCookie=OK
-        // Host:banking.commercebank.com
-        // Origin:https://banking.commercebank.com
-        // Referer:https://banking.commercebank.com/cbi/login.aspx
-        // User-Agent:Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_7) AppleWebKit/534.30 (KHTML, like Gecko) Chrome/12.0.742.77 Safari/534.30
+
       });
     });
   });
