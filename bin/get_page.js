7d67a245fafca03cad6972632b96f3acd364ed90
Stephen R. Veit <srveit@gmail.com>
2011-09-13 09:40:14 -0500

    log page contents

--- bin/get_page.js
+++ bin/get_page.js
@@ -2,7 +2,5 @@ var Mechanize = require('../lib/mechanize');
 
 Mechanize.newAgent().
   get({uri: 'http://www.google.com'}, function (err, page) {
-    //console.log(require('sys').inspect(page));
-    console.log(require('sys').inspect(page.response.headers['content-type']));
-    console.log(page.responseHeaderCharset);
+    console.log(require('sys').inspect(page));
   });
