8f96023e6846bcd016ae9579040bee84ea517467
Stephen R. Veit <srveit@gmail.com>
2011-09-13 09:54:48 -0500

    fix formatting and remove extraneous comments

--- spec/agent_spec.js
+++ spec/agent_spec.js
@@ -57,7 +57,8 @@ describe("Mechanize/Agent", function () {
         response = {
           statusCode: '200',
           headers: {
-            'set-cookie': "sessionid=345; path=/; expires=Fri, 01 Jan 2021 00:00:00 GMT; secure; HttpOnly"
+            'set-cookie': "sessionid=345; path=/; " + 
+              "expires=Fri, 01 Jan 2021 00:00:00 GMT; secure; HttpOnly"
           }
         };
         responseBody = fixture("login.html");
@@ -79,8 +80,12 @@ describe("Mechanize/Agent", function () {
         response = {
           statusCode: '200',
           headers: {
-            'set-cookie': [ "sessionid=345; path=/; expires=Fri, 01 Jan 2021 00:00:00 GMT; secure; HttpOnly",
-                            "name=smith; path=/; expires=Fri, 01 Jan 2021 00:00:00 GMT; secure; HttpOnly"]
+            'set-cookie': [ 
+              "sessionid=345; path=/; " + 
+                "expires=Fri, 01 Jan 2021 00:00:00 GMT; secure; HttpOnly",
+              "name=smith; path=/; " + 
+                "expires=Fri, 01 Jan 2021 00:00:00 GMT; secure; HttpOnly"
+            ]
           }
         };
         responseBody = fixture("login.html");
@@ -162,6 +167,10 @@ describe("Mechanize/Agent", function () {
           requestOptions.headers.Cookie.should.eql('sessionid=123');
         });
 
+        it("should have accept", function () {
+          requestOptions.headers.Accept.should.eql('*/*');
+        });
+
         it("should have body", function () {
           requestOptions.body.should.eql(requestData);
         });
@@ -194,14 +203,6 @@ describe("Mechanize/Agent", function () {
         it("should post form fields in body", function () {
           requestOptions.body.should.eql(requestData);
         });
-
-        // Accept:text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-        // Accept-Charset:ISO-8859-1,utf-8;q=0.7,*;q=0.3
-        // Accept-Encoding:gzip,deflate,sdch
-        // Accept-Language:en-US,en;q=0.8
-        // Cache-Control:max-age=0
-        // Connection:keep-alive
-        // Cookie:__utmz=61007678.1305596160.17.2.utmccn=(referral)|utmcsr=banking.commercebank.com|utmcct=/CBI/login.aspx|utmcmd=referral; PMData=PMV5DWe%2FsxKTBOtqeUNOOdquqzzk7sbQ5DBq7xcybZGU%2FV55aZUkH9ZnDKWCeGaxEpzywJESltItBZ3P2H%2FjAb4Kmq5g%3D%3D; __utma=61007678.1044561854.1294586917.1307199932.1307209924.21; __utmb=61007678; __utmc=61007678; TestCookie=OK
 
       });
     });
