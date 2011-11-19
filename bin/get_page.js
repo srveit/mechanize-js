65355a781d6456a78f518f734aa233eadf9bea42
Stephen R. Veit <srveit@gmail.com>
2011-11-19 13:38:11 -0600

    do not use inspect in console.log

--- bin/get_page.js
+++ bin/get_page.js
@@ -2,5 +2,5 @@ var Mechanize = require('../lib/mechanize');
 
 Mechanize.newAgent().
   get({uri: 'http://www.google.com'}, function (err, page) {
-    console.log(require('sys').inspect(page));
+    console.log(page);
   });
