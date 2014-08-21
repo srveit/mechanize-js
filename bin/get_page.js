8c39a41f92b16eedea1c68c141ea736acc434b26
Dan Rahmel <drahmel7@gmail.com>
2014-08-21 14:08:52 -0700

    DJR:Accept a url from the command line

--- bin/get_page.js
+++ bin/get_page.js
@@ -1,6 +1,12 @@
+var arguments = process.argv.slice(2);
+
 var Mechanize = require('../lib/mechanize');
+var uri = 'http://www.google.com';
+if(arguments.length > 0) {
+	uri = arguments[0];
+}
 
 Mechanize.newAgent().
-  get({uri: 'http://www.google.com'}, function (err, page) {
-    console.log(page);
+  get({uri: uri}, function (err, page) {
+      console.log(page);
   });
