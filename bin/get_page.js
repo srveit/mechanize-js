a4b31310710792f1831df4b4ec27fb0acf7423a6
Stephen Veit <srveit@gmail.com>
2017-10-18 11:04:29 -0500

    udpate new agent creation

--- bin/get_page.js
+++ bin/get_page.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
 'use strict';
+const arguments = process.argv.slice(2),
+      mechanize = require('../lib/mechanize'),
+  uri = 'http://www.google.com';
 
-const mechanize = require('../lib/mechanize'),
-  args = process.argv.slice(2);
-
-let uri = 'http://www.google.com';
-
-if(args.length > 0) {
-  uri = args[0];
+if (arguments.length > 0) {
+  uri = arguments[0];
 }
 
-mechanize.newAgent().
-  get({uri: uri}, function (err, page) {
-    console.log(page);
-  });
+mechanize.newAgent()
+  .get({uri})
+  .then(page => console.log(page));
