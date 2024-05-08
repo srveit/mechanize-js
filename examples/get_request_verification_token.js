5ce6baaba395b1c5d7c2eb84dbf1ca1d603e5360
Stephen Veit <srveit@gmail.com>
2024-05-08 17:58:55 -0500

    fix sending headers to mocks

--- examples/get_request_verification_token.js
+++ examples/get_request_verification_token.js
@@ -7,42 +7,50 @@ const getRequestVerificationToken = async () => {
   const uri = new URL('Investor/#Home', baseUri)
   const agent = newAgent()
   agent.setUserAgent('Mac Safari')
-  const page = await agent.get({
+  const page = await agent.get(
+    uri,
+    {
     redirect: 'manual',
     headers: {
       'Sec-Fetch-Dest': 'document',
       'Sec-Fetch-Mode': 'navigate',
       'Sec-Fetch-Site': 'none',
     },
-    uri,
+    
   })
   console.log('page', page.body)
   console.log('responseHeaders', page.responseHeaders)
-  console.log('response cookies', await agent.getCookies({domain: 'www-us.computershare.com'}))
-
-  const page2 = await agent.get({
+  console.log(
+    'response cookies',
+    await agent.getCookies({ domain: 'www-us.computershare.com' })
+  )
+  return
+  const page2 = await agent.get(
+    '/Investor/mobile/tomobile',
+    {
     redirect: 'manual',
     headers: {
       'Sec-Fetch-Dest': 'document',
       'Sec-Fetch-Mode': 'navigate',
       'Sec-Fetch-Site': 'none',
     },
-    uri: '/Investor/mobile/tomobile',
+    
   })
   console.log('page2', page2.body)
   console.log(page2.responseHeaders)
-  const page3 = await agent.get({
+  const page3 = await agent.get(
+    'http://www-us.computershare.com/Investor/',
+    {
     redirect: 'manual',
     headers: {
       'Sec-Fetch-Dest': 'document',
       'Sec-Fetch-Mode': 'navigate',
       'Sec-Fetch-Site': 'none',
     },
-    uri: 'http://www-us.computershare.com/Investor/',
+    
   })
   console.log('page3', page3.body)
-  console.log(        page3.responseHeaders)
-
+  console.log(page3.responseHeaders)
 }
 
 getRequestVerificationToken()
