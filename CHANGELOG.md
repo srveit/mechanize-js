6ebc4ec23979ba20ad58aa544ee1f3de00e13b35
Stephen Veit <srveit@gmail.com>
2024-05-13 17:35:23 -0500

    update parameters to agent.get

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,11 +5,17 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.5.0] - 2024-05-13
+
+### Changed
+
+- changed options to agent.get
+
 ## [1.4.1] - 2024-04-25
 
 ### Changed
 
-- change package main to lib/mechanize.js
+- changed package main to lib/mechanize.js
 
 ## [1.4.0] - 2024-04-25
 
--- examples/get_request_verification_token.js
+++ examples/get_request_verification_token.js
@@ -4,19 +4,16 @@ const args = process.argv.slice(2)
 const baseUri = new URL('https://www-us.computershare.com/')
 
 const getRequestVerificationToken = async () => {
-  const uri = new URL('Investor/#Home', baseUri)
+  const uri = new URL('Investor/#Home', baseUri).toString()
   const agent = newAgent()
   agent.setUserAgent('Mac Safari')
-  const page = await agent.get(
-    uri,
-    {
+  const page = await agent.get(uri, {
     redirect: 'manual',
     headers: {
       'Sec-Fetch-Dest': 'document',
       'Sec-Fetch-Mode': 'navigate',
       'Sec-Fetch-Site': 'none',
     },
-    
   })
   console.log('page', page.body)
   console.log('responseHeaders', page.responseHeaders)
@@ -24,30 +21,25 @@ const getRequestVerificationToken = async () => {
     'response cookies',
     await agent.getCookies({ domain: 'www-us.computershare.com' })
   )
-  return
-  const page2 = await agent.get(
-    '/Investor/mobile/tomobile',
-    {
+
+  const page2 = await agent.get('/Investor/mobile/tomobile', {
     redirect: 'manual',
     headers: {
       'Sec-Fetch-Dest': 'document',
       'Sec-Fetch-Mode': 'navigate',
       'Sec-Fetch-Site': 'none',
     },
-    
   })
   console.log('page2', page2.body)
   console.log(page2.responseHeaders)
-  const page3 = await agent.get(
-    'http://www-us.computershare.com/Investor/',
-    {
+  return
+  const page3 = await agent.get('http://www-us.computershare.com/Investor/', {
     redirect: 'manual',
     headers: {
       'Sec-Fetch-Dest': 'document',
       'Sec-Fetch-Mode': 'navigate',
       'Sec-Fetch-Site': 'none',
     },
-    
   })
   console.log('page3', page3.body)
   console.log(page3.responseHeaders)
